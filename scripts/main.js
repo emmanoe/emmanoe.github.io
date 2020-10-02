bc9a4fd05ee7d957c911754130359626ad2ff6ec
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2020-10-02 21:47:44 +0200

    Implement game initial cards

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,20 @@ addCardButton.addEventListener('click', function(){
   element.appendChild(newCard);  
   turn++;
 })
+
+function game_initialisation(){
+  let bank_element = document.querySelector('#bank_section');
+  var firstBankCard = document.createElement("img");
+  firstBankCard.setAttribute('src','images/PNG/'+getRandomCardNumber()+'.png');
+  bank_element.appendChild(firstBankCard);
+
+  let player_element = document.querySelector('#player_section');
+  var firstPlayerCard = document.createElement("img");
+  var secondPlayerCard = document.createElement("img");
+  firstPlayerCard.setAttribute('src','images/PNG/'+getRandomCardNumber()+'.png');
+  secondPlayerCard.setAttribute('src','images/PNG/'+getRandomCardNumber()+'.png');
+  player_element.appendChild(firstPlayerCard);
+  player_element.appendChild(secondPlayerCard);
+}
+
+game_initialisation();
